6e205dd1f61b63f9dcdb2d01096faa87ab8f113f
Guilherme <guilhermeavellar25@gmail.com>
2025-05-04 12:54:10 -0300

    Update Cliente Class to support Pessoa

--- src/com/finance/app/Cliente.java
+++ src/com/finance/app/Cliente.java
@@ -1,54 +1,31 @@
 package com.finance.app;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
+public class Cliente extends Pessoa{
 
-public class Cliente {
-
-    private static int proximoId = 1; // usado para atribuir IDs únicos para as instancias de objetos
+    private static int proximoId = 1;
     private int id;
-    private String cpf;
     Conta conta;
-    private String nome;
     private String email;
-    private LocalDate dataNascimento;
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    // data_nascimento deve ser passada no formato yyyy-MM-dd
-    Cliente(String cpf, String nome, String email, String data_nascimento) {
-        this.cpf = cpf;
-        this.nome = nome;
-        this.dataNascimento = LocalDate.parse(data_nascimento);
+    // Construtor 1
+    Cliente(String cpf, String nome, String email, String dataNascimento) {
+        super(cpf, nome, dataNascimento); // yyyy-MM-dd
         this.email = email;
         // this.conta = criarConta(...);
 
         this.id = proximoId++;
     }
 
+    // Construtor 2
+    Cliente(Pessoa pessoa, String email) {
+        super(pessoa.getCpf(), pessoa.getNome(), pessoa.getDataNascimento());
+        this.email = email;
+        this.id = proximoId++;
+    }
 
 
     public void criarConta(String numeroConta, String agencia) {
         this.conta = new Conta(numeroConta, agencia, this);
-    }
-
-
-    public String getCpf() {
-        return cpf;
-    }
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento.format(formatter);
-    }
-
-
-    public int getIdade() {
-        LocalDate hoje = LocalDate.now();
-        Period idade = Period.between(dataNascimento, hoje);
-        return idade.getYears(); // Data atual - Data de nascimento
     }
 
     public String getEmail() {
@@ -60,13 +37,10 @@ public class Cliente {
     public String toString() {
         return "Cliente{" +
                 "id=" + id +
-                ", cpf='" + cpf + '\'' +
-                ", nome='" + nome + '\'' +
                 ", email='" + email + '\'' +
-                ", dataNascimento='" + getDataNascimento() + '\'' +
-                ", idade=" + getIdade() +
-                '}';
+                ", conta=" + conta +
+                "} " +
+                super.toString()
+                ;
     }
-
-
 }
