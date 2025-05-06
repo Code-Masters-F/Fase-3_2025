7bec39b54094bb683a0c2f239cfaa970bec3f9c8
Caio Venancio <161903325+CaioVenxncio@users.noreply.github.com>
2025-05-06 19:47:52 -0300

    Update ContaCliente.java

--- src/com/finance/app/ContaCliente.java
+++ src/com/finance/app/ContaCliente.java
@@ -1,26 +1,59 @@
 package com.finance.app;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContaCliente extends Conta {
 
     private Cliente cliente;
+    private float saldo;
     private List<TransacaoCrypto> transacoesCryptos;
+    private List<TransacaoConta> transacoesContas;
 
-    public ContaCliente(String numeroConta, String agencia, Cliente cliente) {
-        super(numeroConta, agencia);
+    public ContaCliente(String numeroConta, String agencia, LocalDate dataAbertura, int id, Cliente cliente) {
+        super(numeroConta, agencia, dataAbertura, id);
         this.cliente = cliente;
+        this.saldo = 0.0f;
         this.transacoesCryptos = new ArrayList<>();
+        this.transacoesContas = new ArrayList<>();
     }
 
-    public boolean transferirContasCorretora(Conta destino, float valor) {
-        if (valor > 0 && getSaldo() >= valor) {
+    @Override
+    public float getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public boolean depositar(float valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transferirContasCorretora(ContaCliente destino, float valor) {
+        if (valor > 0 && saldo >= valor) {
             destino.receberTransacaoConta(valor, getNumeroConta(), destino.getNumeroConta(), getAgencia(), destino.getAgencia());
             this.transferirParaOutraConta(valor, getNumeroConta(), destino.getNumeroConta(), getAgencia(), destino.getAgencia());
             return true;
         }
         return false;
+    }
+
+    public boolean transferirParaOutraConta(float valor, String contaOrigem, String contaDestino, String agenciaOrigem, String agenciaDestino) {
+        TransacaoConta transacao = new TransacaoConta(valor, contaOrigem, contaDestino, agenciaOrigem, agenciaDestino);
+        saldo -= valor;
+        transacoesContas.add(transacao);
+        return true;
+    }
+
+    public boolean receberTransacaoConta(float valor, String contaOrigem, String contaDestino, String agenciaOrigem, String agenciaDestino) {
+        TransacaoConta transacao = new TransacaoConta(valor, contaOrigem, contaDestino, agenciaOrigem, agenciaDestino);
+        saldo += valor;
+        transacoesContas.add(transacao);
+        return true;
     }
 
     public void adicionarTransacaoCrypto(TransacaoCrypto transacao) {
@@ -29,6 +62,10 @@ public class ContaCliente extends Conta {
 
     public List<TransacaoCrypto> getTransacoesCrypto() {
         return transacoesCryptos;
+    }
+
+    public List<TransacaoConta> getTransacoesContas() {
+        return transacoesContas;
     }
 
     public Cliente getCliente() {
@@ -41,10 +78,10 @@ public class ContaCliente extends Conta {
                 "id=" + getId() +
                 ", numeroConta='" + getNumeroConta() + '\'' +
                 ", agencia='" + getAgencia() + '\'' +
-                ", saldo=" + getSaldo() +
+                ", saldo=" + saldo +
                 ", cliente=" + (cliente != null ? cliente.getNome() : "N/A") +
                 ", transacoesCryptos=" + transacoesCryptos.size() +
-                ", transacoesContas=" + getTransacoesContas().size() +
+                ", transacoesContas=" + transacoesContas.size() +
                 '}';
     }
 }
